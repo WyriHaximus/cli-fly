ab9db9a3ce49da0fd3a5a03cbb8c670ab487c73e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-02-03 23:00:31 +0100

    Started cloning this for cilex

--- src/FlysystemServiceProvider.php
+++ src/FlysystemServiceProvider.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace WyriHaximus\SliFly;
+namespace WyriHaximus\CliFly;
 
-use League\Flysystem\Filesystem;
-use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Cilex\Application;
+use Cilex\ServiceProviderInterface;
 
 class FlysystemServiceProvider implements ServiceProviderInterface
 {
@@ -17,14 +16,7 @@ class FlysystemServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        $app['flysystem.filesystems'] = array();
-        $app['flysystems'] = $app->share(function (Application $app) {
-            $flysystems = new \Pimple();
-            foreach ($app['flysystem.filesystems'] as $alias => $parameters) {
-                $flysystems[$alias] = $this->buildFilesystem($parameters);
-            }
-            return $flysystems;
-        });
+        $this->registerFlysystem($app);
     }
 
     /**
@@ -36,18 +28,5 @@ class FlysystemServiceProvider implements ServiceProviderInterface
      */
     public function boot(Application $app)
     {
-    }
-
-    /**
-     * Instantiate an adapter and wrap it in a filesystem.
-     *
-     * @param array $parameters Array containing the adapter classname and arguments that need to be passed into it.
-     *
-     * @return Filesystem
-     */
-    protected function buildFilesystem(array $parameters)
-    {
-        $adapter = new \ReflectionClass($parameters['adapter']);
-        return new Filesystem($adapter->newInstanceArgs($parameters['args']));
     }
 }
